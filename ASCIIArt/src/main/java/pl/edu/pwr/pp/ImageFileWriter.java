7a715eafd7dcb41502d50b7827bf31ed8ec80ee4
Agata Migalska <agata.migalska@pwr.wroc.pl>
2016-04-28 13:46:31 +0200

    Hint for writing to txt file

--- ASCIIArt/src/main/java/pl/edu/pwr/pp/ImageFileWriter.java
+++ ASCIIArt/src/main/java/pl/edu/pwr/pp/ImageFileWriter.java
@@ -3,7 +3,9 @@ package pl.edu.pwr.pp;
 public class ImageFileWriter {
 
 	public void saveToTxtFile(char[][] ascii, String fileName) {
+		// np. korzystając z java.io.PrintWriter
 		// TODO Wasz kod
+		
 	}
 	
 }
